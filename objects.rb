93e7c0b95552705b2975df53b26aa4b5e2b17eeb
Alex Grant <grantovich@users.noreply.github.com>
2014-06-12 17:28:06 -0400

    Create objects.rb

---
+++ objects.rb
@@ -0,0 +1,25 @@
+#### Question 1
+# Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
+
+
+#### Question 2
+# Copy your class definitions from question 1 here.
+# Then add an initializer to the `Pet` class that accepts a "name" argument
+# and stores the value as an instance variable.
+
+
+#### Question 3
+# Copy your class definitions from question 2 here.
+# Then add an instance method called `bark` to the `Dog` class that returns the
+# string "bark bark".
+
+
+#### Question 4
+# Write code that would create a new instance of the `Dog` class with the
+# name "Lassie", and call the `bark` method on it.
+
+
+#### Question 5
+# Write code that would create a new instance of the `Pet` class with the
+# name "Reginald". Assuming the `Pet` class had a setter for "name", write
+# code that would change Reginald's name to "Snookums".
