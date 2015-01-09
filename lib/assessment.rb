fd7e961d235e6ff96dd2a047df1f2bbbcd7acfdc
David Fisher <tibbon@gmail.com>
2015-01-09 16:36:55 -0500

    added boilerplate files

---
+++ lib/assessment.rb
@@ -0,0 +1,34 @@
+# Question 1
+# This function should take an integer as an argument
+# and output a string that contains that integer that number of times in a row
+# Do not use puts!
+# For example:
+# string_it_x_times(4) #=> "4444"
+# string_it_x_times(2) #=> "22"
+def string_it_x_times(x)
+end
+
+# Question 2
+# This function should take an integer as an argument
+# and will return the string of "fizz" if the number is divisible by 3,
+# it will return "buzz" if it is divisible by 5,
+# it will return "fizzbuzz" if it is divisible by 3 and 5
+# and will return the original integer if it is divisible by neither
+# Do not use puts!
+# For example:
+# fizz_or_buzz(3) #=> "fizz"
+# fizz_or_buzz(10) #=> "buzz"
+# fizz_or_buzz(15) #=> "fizzbuzz"
+# fizz_or_buzz(2) #=> 2
+def fizz_or_buzz(x)
+end
+
+# Question 3
+# This function should take an array and an element of any type
+# The function should return the array, with the element inserted at the end
+# Do not use puts!
+# For example:
+# add_element_to_array([1, 2], 3) #=> [1, 2, 3]
+# add_element_to_array(['fizz', 'buzz'], 'baz') #=> ['fizz', 'buzz', 'baz']
+def add_element_to_array(array, item)
+end
